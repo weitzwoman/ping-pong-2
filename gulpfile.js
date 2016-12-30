072d45ab3dd8f839b231706e00b92fd23e2972bf
Tracie Weitzman <tlweitzman@gmail.com>
2016-12-30 15:15:02 -0800

    add email address to index, signup js, and routing

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
 gulp.task('myTask', function(){
   console.log('hello gulp');
 });
 
-var browserify = require('browserify');
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
 
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/pingpong-interface.js'] })
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
 });
-
-var source = require('vinyl-source-stream');
